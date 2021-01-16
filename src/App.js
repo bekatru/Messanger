ce59b2902a45b8b75beb49d898edbd1e40459b87
bekatru <teamusicband@gmail.com>
2021-01-16 15:43:43 +0300

    branching test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/navbar/Header";
+import ChatItem from "./components/shared/ChatItem";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<ChatItem></ChatItem>
 		</div>
 	);
 }
