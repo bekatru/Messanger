002f0accf435c056714b7f3aa91bfb97f0c39f0f
bekatru <teamusicband@gmail.com>
2021-01-16 15:13:30 +0300

    small components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.scss";
-import Avatar from "./components/shared/Avatar";
 
 function App() {
   return (
     <div className="App">
-      <Avatar />
       <h1>Messanger</h1>
     </div>
   );
--- src/components/shared/Avatar.js
+++ src/components/shared/Avatar.js
@@ -1,17 +1,43 @@
 import React from "react";
-import "./Avatar.scss";
 
-const Avatar = ({ name = "Beka", surname = "Amatakhunov", diameter = 40 }) => {
+const Avatar = ({ name = "Beka", surname = "Amatakhunov", diameter = 23 }) => {
   //data
   const initials =
     name.charAt(0).toUpperCase() + surname.charAt(0).toUpperCase();
+
+  const circle_style = {
+    height: diameter,
+    width: diameter,
+    borderRadius: "50%",
+    backgroundColor: "#007af4",
+    position: "relative",
+  };
+
+  const small_diameter = 23;
+
+  const avatar_icon_style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: diameter,
+    width: diameter,
+  };
+
   // function
   return (
-    <span className="circle">
-      <svg height={diameter} width={diameter}>
-        <circle cx="50%" cy="50%" r={diameter / 2} fill="#007AF4" />
-      </svg>
-      <span className="name"> {initials} </span>
+    <span className="avatar_icon" style={avatar_icon_style}>
+      <div className="circle" style={circle_style}></div>
+      <span
+        className="name"
+        style={
+          diameter === small_diameter
+            ? { fontSize: "9px", color: "white", position: "absolute" }
+            : { fontSize: "16px", color: "white", position: "absolute" }
+        }
+      >
+        {" "}
+        {initials}{" "}
+      </span>
     </span>
   );
 };
